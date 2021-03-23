1a92f25113be8da8f011174e6d41dc4bc91a28eb
Harpal Jadeja <jadejaharpal14@gmail.com>
2021-03-23 09:45:27 +0530

    Added Diff functionality

--- GitDemoLocal/src/main/Calculator.java
+++ GitDemoLocal/src/main/Calculator.java
@@ -6,5 +6,9 @@ public class Calculator {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 	}
+	
+	public static void diff(int num1, int num2) {
+		System.out.println(num1 - num2);
+	}
 
 }
